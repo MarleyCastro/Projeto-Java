91283701c2928cc06d5d5dcc79151e23a097342c
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-11-26 20:26:25 -0300

    Refactor temperature input and conversion logic 🚀
    Updated temperature input handling and conversion method.

--- Temperature/Main.java
+++ Temperature/Main.java
@@ -1,14 +1,16 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite a temperatura em Celsius: ");
+        double temperaturaCelsius = sc.nextDouble();
+        
         Temperature temperature = new Temperature();
-        temperature.temperature = sc.nextDouble();
-        temperature.comverterTemperature();
+        temperature.temperaturaEmCelsius = temperaturaCelsius;
+        temperature.converterTemperatura();
+        
+        sc.close();
     }
 }
-
-
