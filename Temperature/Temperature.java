34d946695915233ece5a3ab7cc3a0d860b85eb9e
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-11-26 20:25:58 -0300

    Update Temperature.java

--- Temperature/Temperature.java
+++ Temperature/Temperature.java
@@ -1,9 +1,17 @@
+
 public class Temperature {
-    public  double temperature;
+    public double temperaturaEmCelsius;
 
     // A fórmula para converter temperaturas de graus Celsius para Fahrenheit
-    public void comverterTemperature(){
-       int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+    public void converterTemperatura() {
+        // Aplica a fórmula: (temperatura * 1.8) + 32
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+        
+        // Exibe a temperatura com casas decimais
+        System.out.println("A temperatura em Fahrenheit é: " + temperaturaEmFahrenheit);
+        
+        // Casting para inteiro (remove as casas decimais)
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
         System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
     }
 }
