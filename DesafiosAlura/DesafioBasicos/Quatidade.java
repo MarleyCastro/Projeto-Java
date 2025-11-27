351d48e6872f0dfa93aebf93e9208e324801c0d3
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-11-27 14:16:09 -0300

    Create Quatidade.java

---
+++ DesafiosAlura/DesafioBasicos/Quatidade.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int precoProduto = 10;
+        int sum = precoProduto * 3;
+        System.out.println("result: " + sum);
+    }
+}
