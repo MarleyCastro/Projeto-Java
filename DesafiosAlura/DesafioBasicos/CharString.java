105e5c904888aa795234367a4daebc9928815248
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-11-27 14:11:32 -0300

    Create: CharString.java

---
+++ DesafiosAlura/DesafioBasicos/CharString.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        char mensagemChar;
+        mensagemChar = 'H';
+        String mensagemString = " World";
+
+        System.out.println(mensagemChar + mensagemString);
+    }
+}
