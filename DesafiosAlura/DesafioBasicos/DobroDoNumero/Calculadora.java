ebb9a018dcb27e37f921ce40680c38ab363983f6
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-12-04 15:32:19 -0300

    Add files via upload

---
+++ DesafiosAlura/DesafioBasicos/DobroDoNumero/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public double numero;
+
+
+    void exibir() {
+        double dobroDoNumero = numero * 2;
+        System.out.println("Dobro do numero: " + dobroDoNumero);
+    }
+}
