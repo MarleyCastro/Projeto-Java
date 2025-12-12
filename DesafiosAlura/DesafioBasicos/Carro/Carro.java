0ab00cb5fc62b0b063fe87e66ec188f6ba32430b
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-12-12 15:04:51 -0300

    Add files via upload

--- DesafiosAlura/DesafioBasicos/Carro/Carro.java
+++ DesafiosAlura/DesafioBasicos/Carro/Carro.java
@@ -1,21 +1,93 @@
-import java.util.Date;
-
 public class Carro {
-    public String modelo;
-    public String cor;
-    public int ano;
+    private String modelo;
+    private double precoAno1;
+    private double precoAno2;
+    private double precoAno3;
+
+    public void definirModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void media() {
+        double mediaTotal =  (precoAno1 + precoAno2 + precoAno3) / 3;
+        System.out.println("O resultado da média e: " + mediaTotal);
+    }
 
 
-    void exibirFixaTecnica() {
-        System.out.println("=".repeat(50));
+    public Carro(String modelo, double precoAno1, double precoAno2, double precoAno3) {
+        this.modelo = modelo;
+        this.precoAno1 = precoAno1;
+        this.precoAno2 = precoAno2;
+        this.precoAno3 = precoAno3;
+    }
+
+    public void exibirInfo() {
         System.out.println("Modelo: " + modelo);
-        System.out.println("Cor: " + cor);
-        System.out.println("Ano: " + ano);
-        System.out.println("Idade do carro: " + calcularIdade());
+        System.out.println("Preço Ano 1: " + precoAno1);
+        System.out.println("Preço Ano 2: " + precoAno2);
+        System.out.println("Preço Ano 3: " + precoAno3);
+        System.out.println("Menor Preço: " + calcularMenorPreco());
+        System.out.println("Maior Preço: " + calcularMaiorPreco());
     }
 
-    int calcularIdade() {
-        return 2025 - ano ;
+    private double calcularMenorPreco() {
+        double maiorPreco = precoAno1;
+
+        if (precoAno2 < maiorPreco) {
+            maiorPreco = precoAno2;
+        }
+
+        if (precoAno3 < maiorPreco) {
+            maiorPreco = precoAno3;
+        }
+
+        return maiorPreco;
     }
 
+    private double calcularMaiorPreco() {
+        double maiorPreco = precoAno1;
+
+        if (precoAno2 > maiorPreco) {
+            maiorPreco = precoAno2;
+        }
+
+        if (precoAno3 > maiorPreco) {
+            maiorPreco = precoAno3;
+        }
+
+        return maiorPreco;
+    }
+
+
+    public String getNome() {
+        return modelo;
+    }
+
+    public void setNome(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public double getPrecoAno1() {
+        return precoAno1;
+    }
+
+    public void setPrecoAno1(double precoAno1) {
+        this.precoAno1 = precoAno1;
+    }
+
+    public double getPrecoAno2() {
+        return precoAno2;
+    }
+
+    public void setPrecoAno2(double precoAno2) {
+        this.precoAno2 = precoAno2;
+    }
+
+    public double getPrecoAno3() {
+        return precoAno3;
+    }
+
+    public void setPrecoAno3(double precoAno3) {
+        this.precoAno3 = precoAno3;
+    }
 }
--- DesafiosAlura/DesafioBasicos/Carro/Main.java
+++ DesafiosAlura/DesafioBasicos/Carro/Main.java
@@ -1,25 +1,12 @@
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Carro carro = new Carro();
-        System.out.print("Qual o nome modelo: ");
-        carro.modelo = sc.nextLine();
-
-
-        System.out.print("Qual a cor do carro: ");
-        carro.cor = sc.nextLine();
-
-        System.out.print("Qual o ano do carro: ");
-        carro.ano = sc.nextInt();
-
-
-        carro.exibirFixaTecnica();
-
-        sc.close();
+        Carro carro = new Carro("gol", 1800, 1500, 1600);
+        carro.media();
+        carro.definirModelo("sedan");
+        carro.exibirInfo();
     }
 }
