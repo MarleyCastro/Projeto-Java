1f4c509ed06b4d846336fca4490d904fcb95a5d9
Marley Castro <162909728+MarleyCastro@users.noreply.github.com>
2025-12-27 18:38:58 -0300

    Add files via upload

--- DesafiosAlura/DesafioBasicos/Main.java
+++ DesafiosAlura/DesafioBasicos/Main.java
@@ -1,15 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Filme filme = new Filme();
-        filme.nome = "Prova de fogo";
-        filme.anoDeLancamento = 2000;
+        Pessoa p = new Pessoa("Marley", 19);
+        System.out.println(p.toString());
 
-        filme.exibeFichaTecnica();
-        filme.avalia(8);
-        filme.avalia(5);
-        filme.avalia(10);
-        System.out.println(filme.somaDasAvaliacoes);
-        System.out.println(filme.totalAvaliacoes);
-        System.out.println(filme.pegaMedia());
     }
 }
---
+++ DesafiosAlura/DesafioBasicos/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Seu nome é: " + nome +
+                "\nSua idade é: " + idade;
+    }
+}
